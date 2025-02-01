83b5c99d2cd612a67b46da8e1c98d7d832717e58
xFeWo <x_feWo_x@gmx.de>
2025-02-01 23:41:07 +0100

    added cookie constent window

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const consentBanner = document.getElementById("simple-cookie-consent");
+    const allowButton = document.querySelector(".cookie-consent-allow");
+    const denyButton = document.querySelector(".cookie-consent-deny");
+
+    //if (localStorage.getItem("cookieConsent") !== null) {
+    //    consentBanner.style.display = "none";
+    //}
+
+    allowButton.addEventListener("click", function () {
+        localStorage.setItem("cookieConsent", "true");
+        enableCookies();
+        consentBanner.style.display = "none";
+    });
+
+    denyButton.addEventListener("click", function () {
+        localStorage.setItem("cookieConsent", "false");
+        disableCookies();
+        consentBanner.style.display = "none";
+    });
+
+    function enableCookies() {
+        console.log("Alle Cookies erlaubt");
+    }
+
+    function disableCookies() {
+        console.log("Nur notwendige Cookies erlaubt");
+    }
+});
